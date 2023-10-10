04a9d95b1e92e961916b12f156b7fb03a14ab48f
maria garces <mariavgarcesg@gmail.com>
2023-10-10 13:53:43 -0400

    j

--- Prueba1.py
+++ Prueba1.py
@@ -1,9 +1,11 @@
+
 import pandas as pd
 
-def leer_csv_y_crear_dataframe(archivo_csv):
+def readExcel(df):
     try:
         # Lee el archivo CSV y carga su contenido en un DataFrame
-        df = pd.read_csv(archivo_csv)
+        df = pd.read_csv(r"C:\\Users\\MARIA GARCES\\Downloads\\Ratios.csv")
+        # Devuelve el DataFrame
         return df
     except Exception as e:
         print(f"Error al leer el archivo CSV: {str(e)}")
@@ -11,8 +13,8 @@ def leer_csv_y_crear_dataframe(archivo_csv):
 
 # Ejemplo de uso de la función
 if __name__ == "__main__":
-    archivo_csv = "C:\\Users\\MARIA GARCES\\Downloads\\Ratios.csv"  # Reemplaza con la ruta completa al archivo CSV
-    dataframe = leer_csv_y_crear_dataframe(archivo_csv)
+    #archivo_csv = "C:\\Users\\MARIA GARCES\\Downloads\\nombre_del_archivo.csv"  # Reemplaza con la ruta completa al archivo CSV
+    dataframe = readExcel("C:\\Users\\MARIA GARCES\\Downloads\\Ratios.csv")
     
     if dataframe is not None:
         print("DataFrame creado con éxito:")
